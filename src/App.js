810448057ef8acdc53302accc0d4bd09bbba811e
Danielly Freitas <daniellyfreitasc@hotmail.com>
2022-02-16 11:08:46 -0300

    Initial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Simulador de Investimentos
+        </p>
+        <a
+          className="simulador"
+        >
+          Simulador
+        </a>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
